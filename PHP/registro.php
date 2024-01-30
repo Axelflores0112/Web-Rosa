bfa4f92bc4ba212c3e2a77a2da2c392c0d2ecb91
Axelflores0112 <axel.floresb@hotmail.com>
2024-01-30 10:37:37 -0600

    registro acabado

--- PHP/registro.php
+++ PHP/registro.php
@@ -7,20 +7,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // Verificar si se ha enviado el for
     $nombre = $_POST["nombre"];
     $clave = $_POST["clave"];
     $correo = $_POST["correo"];
+    
 
     $database = new Data_base(); //Conexion a base de datos
-
-    if (!empty('nombre') && !empty('clave') && !empty('correo')) {
-        //Insercion a la base de datos
-        $sql = "INSERT INTO usuarios (nombre,clave,correo) VALUES (?.?.?)"; //Aqui solamnete estamos preparando la consulta
-        //Consulta preparada con los parametros
-        $parametros = ['sss', $nombre, $clave, $correo]; //<-esta variable es un array que contiene los valores reales que se insertarán en la consulta SQL.
-        //Insertamos los datos
-        $database->register($sql, $parametros);
-
-        //Cerrar conexion
-        $database->close();
+    try{
+        if (!empty('nombre') && !empty('clave') && !empty('correo')) {
+            //Insercion a la base de datos
+            $sql = "INSERT INTO usuarios (nombre,clave,correo) VALUES (?,?,?)"; //Aqui solamnete estamos preparando la consulta
+            //Consulta preparada con los parametros
+            $parametros = ['sss', $nombre, $clave, $correo]; //<-esta variable es un array que contiene los valores reales que se insertarán en la consulta SQL.
+            //Insertamos los datos
+            $database->register($sql, $parametros);
+    
+            //Cerrar conexion
+            $database->close();
+            header("Location: ../HTML/Presentacion.html");
+            exit;
+        }
+    }catch(Exception $error){
+        die($error->getMessage());
     }
 }
 
-?>
